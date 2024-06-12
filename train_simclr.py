acac6be0a2095fa5f4fd31fbe02069c115a3a5e6
Luka Druzijanic <lukadruzijanic00@gmail.com>
2024-06-12 08:57:44 +0200

    .

---
+++ train_simclr.py
@@ -0,0 +1,361 @@
+import os
+import time
+
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+import torch
+import torch.nn as nn
+from torchvision.datasets.vision import VisionDataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from datasets import prepare_poison_dataset
+from simclr import SimClrBackbone
+
+from torch.optim.optimizer import Optimizer, required
+import re
+
+device = "cuda"
+epochs = 250
+batch_size = 256
+
+
+
+DATASET = "badnets1-1"
+LOAD_CHECKPOINT = True
+CHECKPOINT_NAME = f"{DATASET}-SimCLR-NEW.pt"
+
+
+
+poison_dataset, poison_indices, target_class, dataset = prepare_poison_dataset(DATASET, train=True)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+class ConstrastiveDataset(VisionDataset):
+
+    def __init__(self, original_dataset: VisionDataset, s: int = 0.5):
+        self.original_dataset = original_dataset
+
+        self.transforms = transforms.Compose([
+            transforms.RandomHorizontalFlip(0.5),
+            transforms.RandomResizedCrop(32,(0.8,1.0),antialias=False),
+            transforms.Compose([transforms.RandomApply([
+                transforms.ColorJitter(0.8*s, 0.8*s, 0.8*s, 0.2*s)], p = 0.8),
+                transforms.RandomGrayscale(p=0.2)]),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+            ])
+    
+    def __len__(self):
+        return len(self.original_dataset)
+    
+    def __getitem__(self, index):
+        img = self.original_dataset[index][0]
+        augmented_img_1 = self.transforms(img)
+        augmented_img_2 = self.transforms(img)
+        return augmented_img_1, augmented_img_2
+    
+constrastive_dataset = ConstrastiveDataset(poison_dataset)
+
+model = SimClrBackbone()
+model = model.to(device)
+
+
+class LARS(Optimizer):
+    """
+    Layer-wise Adaptive Rate Scaling for large batch training.
+    Introduced by "Large Batch Training of Convolutional Networks" by Y. You,
+    I. Gitman, and B. Ginsburg. (https://arxiv.org/abs/1708.03888)
+    """
+
+    def __init__(
+        self,
+        params,
+        lr=required,
+        momentum=0.9,
+        use_nesterov=False,
+        weight_decay=0.0,
+        exclude_from_weight_decay=None,
+        exclude_from_layer_adaptation=None,
+        classic_momentum=True,
+        eeta=0.001,
+    ):
+        """Constructs a LARSOptimizer.
+        Args:
+        lr: A `float` for learning rate.
+        momentum: A `float` for momentum.
+        use_nesterov: A 'Boolean' for whether to use nesterov momentum.
+        weight_decay: A `float` for weight decay.
+        exclude_from_weight_decay: A list of `string` for variable screening, if
+            any of the string appears in a variable's name, the variable will be
+            excluded for computing weight decay. For example, one could specify
+            the list like ['batch_normalization', 'bias'] to exclude BN and bias
+            from weight decay.
+        exclude_from_layer_adaptation: Similar to exclude_from_weight_decay, but
+            for layer adaptation. If it is None, it will be defaulted the same as
+            exclude_from_weight_decay.
+        classic_momentum: A `boolean` for whether to use classic (or popular)
+            momentum. The learning rate is applied during momeuntum update in
+            classic momentum, but after momentum for popular momentum.
+        eeta: A `float` for scaling of learning rate when computing trust ratio.
+        name: The name for the scope.
+        """
+
+        self.epoch = 0
+        defaults = dict(
+            lr=lr,
+            momentum=momentum,
+            use_nesterov=use_nesterov,
+            weight_decay=weight_decay,
+            exclude_from_weight_decay=exclude_from_weight_decay,
+            exclude_from_layer_adaptation=exclude_from_layer_adaptation,
+            classic_momentum=classic_momentum,
+            eeta=eeta,
+        )
+
+        super(LARS, self).__init__(params, defaults)
+        self.lr = lr
+        self.momentum = momentum
+        self.weight_decay = weight_decay
+        self.use_nesterov = use_nesterov
+        self.classic_momentum = classic_momentum
+        self.eeta = eeta
+        self.exclude_from_weight_decay = exclude_from_weight_decay
+        # exclude_from_layer_adaptation is set to exclude_from_weight_decay if the
+        # arg is None.
+        if exclude_from_layer_adaptation:
+            self.exclude_from_layer_adaptation = exclude_from_layer_adaptation
+        else:
+            self.exclude_from_layer_adaptation = exclude_from_weight_decay
+
+    def step(self, epoch=None, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        if epoch is None:
+            epoch = self.epoch
+            self.epoch += 1
+
+        for group in self.param_groups:
+            weight_decay = group["weight_decay"]
+            momentum = group["momentum"]
+            eeta = group["eeta"]
+            lr = group["lr"]
+
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+
+                param = p.data
+                grad = p.grad.data
+
+                param_state = self.state[p]
+
+                # TODO: get param names
+                # if self._use_weight_decay(param_name):
+                grad += self.weight_decay * param
+
+                if self.classic_momentum:
+                    trust_ratio = 1.0
+
+                    # TODO: get param names
+                    # if self._do_layer_adaptation(param_name):
+                    w_norm = torch.norm(param)
+                    g_norm = torch.norm(grad)
+
+                    device = g_norm.get_device()
+                    trust_ratio = torch.where(
+                        w_norm.gt(0),
+                        torch.where(
+                            g_norm.gt(0),
+                            (self.eeta * w_norm / g_norm),
+                            torch.Tensor([1.0]).to(device),
+                        ),
+                        torch.Tensor([1.0]).to(device),
+                    ).item()
+
+                    scaled_lr = lr * trust_ratio
+                    if "momentum_buffer" not in param_state:
+                        next_v = param_state["momentum_buffer"] = torch.zeros_like(
+                            p.data
+                        )
+                    else:
+                        next_v = param_state["momentum_buffer"]
+
+                    next_v.mul_(momentum).add_(scaled_lr, grad)
+                    if self.use_nesterov:
+                        update = (self.momentum * next_v) + (scaled_lr * grad)
+                    else:
+                        update = next_v
+
+                    p.data.add_(-update)
+                else:
+                    raise NotImplementedError
+
+        return loss
+
+    def _use_weight_decay(self, param_name):
+        """Whether to use L2 weight decay for `param_name`."""
+        if not self.weight_decay:
+            return False
+        if self.exclude_from_weight_decay:
+            for r in self.exclude_from_weight_decay:
+                if re.search(r, param_name) is not None:
+                    return False
+        return True
+
+    def _do_layer_adaptation(self, param_name):
+        """Whether to do layer-wise learning rate adaptation for `param_name`."""
+        if self.exclude_from_layer_adaptation:
+            for r in self.exclude_from_layer_adaptation:
+                if re.search(r, param_name) is not None:
+                    return False
+        return True
+    
+
+class SimCLR_Loss(nn.Module):
+    def __init__(self, temperature):
+        super(SimCLR_Loss, self).__init__()
+        self.temperature = temperature
+
+        self.criterion = nn.CrossEntropyLoss(reduction="sum")
+        self.similarity_f = nn.CosineSimilarity(dim=2)
+
+    def mask_correlated_samples(self, batch_size):
+        N = 2 * batch_size
+        mask = torch.ones((N, N), dtype=bool)
+        mask = mask.fill_diagonal_(0)
+        
+        for i in range(batch_size):
+            mask[i, batch_size + i] = 0
+            mask[batch_size + i, i] = 0
+            
+        return mask
+
+    def forward(self, z_i, z_j):
+        """
+        We do not sample negative examples explicitly.
+        Instead, given a positive pair, similar to (Chen et al., 2017), we treat the other 2(N − 1) augmented examples within a minibatch as negative examples.
+        """
+        batch_size = z_i.shape[0]
+        mask = self.mask_correlated_samples(batch_size)
+
+        N = 2 * batch_size #* self.world_size
+        
+        #z_i_ = z_i / torch.sqrt(torch.sum(torch.square(z_i),dim = 1, keepdim = True))
+        #z_j_ = z_j / torch.sqrt(torch.sum(torch.square(z_j),dim = 1, keepdim = True))
+
+        z = torch.cat((z_i, z_j), dim=0)
+
+        sim = self.similarity_f(z.unsqueeze(1), z.unsqueeze(0)) / self.temperature
+        
+        #print(sim.shape)
+
+        sim_i_j = torch.diag(sim, batch_size)
+        sim_j_i = torch.diag(sim, batch_size)
+        
+        
+        # We have 2N samples, but with Distributed training every GPU gets N examples too, resulting in: 2xNxN
+        positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(N, 1)
+        negative_samples = sim[mask].reshape(N, -1)
+        
+        
+        #SIMCLR
+        labels = torch.from_numpy(np.array([0]*N)).reshape(-1).to(positive_samples.device).long() #.float()
+        #labels was torch.zeros(N)
+        logits = torch.cat((positive_samples, negative_samples), dim=1)
+        loss = self.criterion(logits, labels)
+        loss /= N
+        
+        return loss
+    
+
+optimizer = LARS(
+    [params for params in model.parameters() if params.requires_grad],
+    lr=0.2,
+    weight_decay=1e-6,
+    exclude_from_weight_decay=["batch_normalization", "bias"],
+)
+
+# "decay the learning rate with the cosine decay schedule without restarts"
+warmupscheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lambda epoch : (epoch+1)/10.0, verbose = False)
+mainscheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 500, eta_min=0.05, last_epoch=-1, verbose = False)
+
+criterion = SimCLR_Loss(temperature=0.5)
+
+dataloader = DataLoader(constrastive_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+dataloader_no_contrastive = DataLoader(poison_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+def train(dataloader, model, criterion, optimizer):
+    loss_epoch = 0
+    
+    for step, (x_i, x_j) in enumerate(dataloader):
+        optimizer.zero_grad()
+        x_i = x_i.to(device).float()
+        x_j = x_j.to(device).float()
+
+        # positive pair, with encoding
+        z_i = model(x_i)
+        z_j = model(x_j)
+
+        loss = criterion(z_i, z_j)
+        loss.backward()
+
+        optimizer.step()
+        
+        if step % 50 == 0:
+            print(f"\tStep [{step}/{len(dataloader)}]\t Loss: {round(loss.item(), 5)}")
+
+        loss_epoch += loss.item()
+    return loss_epoch / len(dataloader)
+
+def save_model(model, optimizer, scheduler, epoch, name):
+    out = os.path.join('./saved_models/new/', name)
+
+    torch.save({'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'scheduler_state_dict': scheduler.state_dict(),
+                'epoch': epoch
+                }, out)
+
+    print(f"\tSaved model, optimizer, scheduler and epoch info to {out}")
+
+start_epoch = 0
+
+load_checkpoint = False
+
+if LOAD_CHECKPOINT:
+    out = os.path.join('./saved_models/new', CHECKPOINT_NAME)
+    checkpoint = torch.load(out, map_location=device)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    mainscheduler.load_state_dict(checkpoint["scheduler_state_dict"])
+    start_epoch = checkpoint["epoch"] + 1
+
+
+losses = []
+
+for epoch in range(start_epoch, epochs+1):
+    print(f"Epoch [{epoch}/{epochs}]\t")
+    stime = time.time()
+
+    model.train()
+    loss = train(dataloader, model, criterion, optimizer)
+    losses.append(loss)
+
+    if epoch <= 10:
+        warmupscheduler.step()
+    if epoch > 10:
+        mainscheduler.step()
+    
+    print()
+    print(f"\tTraining Loss: {loss}")
+    time_taken = (time.time()-stime)/60
+    print(f"\tTime Taken: {time_taken} minutes")
+
+    save_model(model, optimizer, mainscheduler, epoch, f"{DATASET}-SimCLR-NEW.pt")
+
+## end training
+save_model(model, optimizer, mainscheduler, epochs, f"{DATASET}-SimCLR-NEW.pt")
---
+++ util.py
@@ -0,0 +1,274 @@
+import os
+
+from math import ceil, floor
+
+import copy
+
+import tqdm
+
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision.datasets.vision import VisionDataset
+
+from sklearn.neighbors import KNeighborsClassifier
+
+from scipy.stats import multivariate_normal
+
+from sklearn.cluster import KMeans
+from scipy.spatial.distance import euclidean
+
+from sklearn.manifold import TSNE
+import umap
+
+from simclr import SimClrBackbone
+
+DEVICE = "cuda"
+
+def load_simclr(simclr_model_name: str) -> SimClrBackbone:
+    model = SimClrBackbone()
+    out = os.path.join('./saved_models/simclr/', simclr_model_name)
+    checkpoint = torch.load(out, map_location=DEVICE)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model = model.to(DEVICE)
+    model.eval()
+    epochs = checkpoint["epoch"]
+    return model, epochs
+
+
+
+def extract_simclr_features(model: SimClrBackbone, dataset: VisionDataset, layer: str = "repr"):
+
+    assert layer in ["loss", "repr"]
+
+    simclr_feature_size = 128 if layer == "loss" else 512
+    num_examples = len(dataset)
+
+    features = np.zeros((num_examples, simclr_feature_size))
+    labels_poison = np.zeros((num_examples))
+    labels_true = np.zeros((num_examples))
+
+    batch_size = 256
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+    for i, (img, labels_batch_poison, labels_batch_true) in enumerate(dataloader):
+
+        with torch.no_grad():
+            if layer == "loss":
+                features_batch = model(img.to(DEVICE)).cpu().data.numpy()
+            elif layer == "repr":
+                features_batch = model.forward_repr(img.to(DEVICE)).cpu().data.numpy()
+            
+        features[i*batch_size : i*batch_size+len(features_batch)] = features_batch
+        labels_poison[i*batch_size : i*batch_size+len(labels_batch_poison)] = labels_batch_poison.long()
+        labels_true[i*batch_size : i*batch_size+len(labels_batch_true)] = labels_batch_true.long()
+
+    labels_poison = labels_poison.astype(int)
+    labels_true = labels_true.astype(int)
+
+    return features, labels_poison, labels_true
+
+
+
+def calculate_features_2d(features: np.array, n_neighbors: int = 100, algorithm: str = "umap", min_dist: float = 0.1) -> np.array:
+    assert algorithm in ["umap", "tsne"]
+    
+    if algorithm == "umap":
+        alg = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist)
+    elif algorithm == "tsne":
+        alg = TSNE(n_components = 2, perplexity = n_neighbors)
+    features_2d = alg.fit_transform(features)
+    return features_2d
+
+def plot_features_2d(features_2d: np.array, labels: np.array, poison_indices: np.array, legend: bool = True, title: str = None, show: bool = True) -> None:
+    num_classes = int(max(labels).item())
+
+    # label poison examples as 10
+    labels_10 = copy.deepcopy(labels)
+    labels_10[poison_indices] = 10
+
+    for i in range(num_classes+1):
+        plt.scatter(features_2d[labels_10==i,1], features_2d[labels_10==i,0], s=7)
+    plt.scatter(features_2d[labels_10==10,1], features_2d[labels_10==10,0], c = "black", marker= "x", s=1)
+
+    if title: plt.title(title)
+
+    if legend: plt.legend([str(i) for i in range(num_classes+1)] + ["poison"])
+    
+    if show: plt.show()
+
+def calculate_and_plot_features_2d(features: np.array, labels: np.array, poison_indices: np.array, subset_size: int = None, legend: bool = True) -> np.array:
+    # Plot only a subset
+    if subset_size is None:
+        subset_size = len(features)
+    features_subset = features[:subset_size]
+    labels_subset = labels[:subset_size]
+    poison_indices_subset = poison_indices[:subset_size]
+    
+    features_2d = calculate_features_2d(features_subset)
+    plot_features_2d(features_2d, labels_subset, poison_indices_subset, legend=legend)
+
+
+def print_evaluate_cleanse(poison_predicted: np.array, poison_indices: np.array, verbose: bool = True) -> float:
+
+    tp = (poison_indices & poison_predicted).sum()
+    fp = (np.invert(poison_indices) & poison_predicted).sum()
+    fn = (poison_indices & np.invert(poison_predicted)).sum()
+    tn = (np.invert(poison_indices) & np.invert(poison_predicted)).sum()
+
+    fnr = fn/(fn+tp) if fn+tp!=0 else 0
+    tnr = tn/(tn+fp) if tn+fp!=0 else 0
+    poison_rate = fn/(fn+tn) if fn+tn!=0 else 0
+
+    if verbose:
+        print(f"{tp} \t {fp}")
+        print(f"{fn} \t {tn}")
+        print(f"Percentage of poisoned images (out of all poisoned) kept: {100*fnr: .2f}%")
+        print(f"Percentage of clean images (out of all clean) kept: {100*tnr: .2f}%")
+        print(f"Percentage of remaining poisoned images (out of all remaining): {100*poison_rate: .2f}%")
+
+def evaluate_cleanse(poison_predicted: np.array, poison_indices: np.array, verbose: bool = True) -> float:
+
+    tp = (poison_indices & poison_predicted).sum()
+    fp = (np.invert(poison_indices) & poison_predicted).sum()
+    fn = (poison_indices & np.invert(poison_predicted)).sum()
+    tn = (np.invert(poison_indices) & np.invert(poison_predicted)).sum()
+
+    fnr = fn/(fn+tp) if fn+tp!=0 else 0
+    tnr = tn/(tn+fp) if tn+fp!=0 else 0
+    poison_rate = fn/(fn+tn) if fn+tn!=0 else 0
+
+    return poison_rate, fnr, tnr
+
+def plot_histogram_poisoned(values: np.array, poison_indices: np.array = None, is_integer: bool = False, bins_num: int = 100, separation_line: float = None,
+                            x_axis_label: str = None, y_axis_label: str = None, show: bool = True, legend: bool = True
+                            ) -> None:
+    if poison_indices is not None:
+        values_clean = values[np.invert(poison_indices)]
+        values_poisoned = values[poison_indices]
+    else:
+        values_clean = values[:]
+        values_poisoned = []
+
+    bins = np.linspace(floor(np.min(values)), ceil(np.max(values)), int(np.max(values)) if is_integer else bins_num)
+    plt.hist(values_clean, bins, alpha=0.5, label='čisti')
+    plt.hist(values_poisoned, bins, alpha=0.5, label='otrovani')
+
+    plt.ylabel(y_axis_label)
+    plt.xlabel(x_axis_label)
+
+    if separation_line:
+        plt.axvline(separation_line, color='red', linestyle='dashed', linewidth=1)
+
+    if legend: plt.legend(loc='upper right')
+    
+    if show: plt.show()
+
+
+def knn_cleanse(features: np.array, labels_poison: np.array, n_neighbors: int) -> np.array:
+    knn = KNeighborsClassifier(n_neighbors)
+    knn.fit(features, labels_poison)
+    labels_predicted = knn.predict(features)
+
+    return labels_predicted != labels_poison
+
+
+def gauss_cleanse(features: np.array, discard_percentage: float, poison_indices: np.array = None) -> np.array:
+    mean = np.mean(features, axis=0)
+    cov = np.cov(features, rowvar=0)
+
+    probabilities = multivariate_normal.pdf(features, mean=mean, cov=cov, allow_singular=True)
+    probabilities[probabilities <= 0] = 1e-100
+    probabilities = -np.log(probabilities)
+
+    discard_line = np.percentile(probabilities, (1-discard_percentage)*100)
+    plot_histogram_poisoned(probabilities, poison_indices, separation_line=discard_line)
+
+    predicted_poison_indices = probabilities > discard_line
+    return predicted_poison_indices
+
+
+def kmeans_cleanse(features: np.array, means: int = 50, mode: str = "both") -> np.array:
+
+	assert mode in ["distance", "size", "both"]
+
+	kmeans = KMeans(n_clusters=means, init="k-means++")
+	kmeans.fit(features)
+	
+	if mode == "distance":
+		centroid = np.mean(features, axis=0)
+		cluster_center_distances = [euclidean(center, centroid) for center in kmeans.cluster_centers_]
+		poison_cluster_index = cluster_center_distances.index(max(cluster_center_distances))
+	
+	elif mode == "size":
+		predicted_cluster = kmeans.predict(features)
+		_, counts = np.unique(predicted_cluster, return_counts=True)
+		poison_cluster_index = np.argmin(counts)
+	
+	elif mode == "both":
+		centroid = np.mean(features, axis=0)
+		cluster_center_distances = [euclidean(center, centroid) for center in kmeans.cluster_centers_]
+		poison_cluster_index_1 = cluster_center_distances.index(max(cluster_center_distances))
+
+		predicted_cluster = kmeans.predict(features)
+		_, counts = np.unique(predicted_cluster, return_counts=True)
+		poison_cluster_index_2 = np.argmin(counts)
+
+		if poison_cluster_index_1 != poison_cluster_index_2:
+			# No poison detected
+			return np.zeros(features.shape[0]).astype(bool)
+		else:
+			poison_cluster_index = poison_cluster_index_1
+
+	predicted_poison_indices = kmeans.predict(features) == poison_cluster_index
+	return predicted_poison_indices
+
+
+class EnergyClassifier():
+
+    def __init__(self, t=1):
+        self.t = t
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        
+        self.C = int(np.max(y))
+        self.Ic = {c:[i for i in range(len(y)) if y[i]==c] for c in range(self.C)}
+        
+    def predict_index(self, i):
+        # consider improving with numpy and batch
+
+        xi = self.X[i]
+
+        exp_all = np.exp([xi*self.X[k]/self.t for k in range(len(self.X))])
+        sum_exp_all_except_xi = np.sum([exp_all[k] for k in range(len(self.X)) if k!=i])
+        mean_exp_c = [np.mean([exp_all[k] for k in self.Ic[c] if k!=i]) for c in range(self.C)]
+    
+        Scs = mean_exp_c / sum_exp_all_except_xi
+        return np.argmax(Scs)
+
+    def predict(self):
+        predicted = np.zeros((len(self.X)))
+        for i in tqdm(range(len(self.X))):
+            predicted[i] = self.predict_index(i)
+        return predicted
+    
+def energy_cleanse(features: np.array, labels_poison: np.array, t: float = 10) -> np.array:
+    
+    # if DATASET == "badnets":
+    #     T = 100
+    # elif DATASET == "wanet":
+    #     T = 10
+    # elif DATASET == "sig":
+    #     T = 1
+    # else:
+    #     raise Exception("Invalid dataset")
+
+    energy = EnergyClassifier(t=t)
+    energy.fit(features, labels_poison)
+    labels_predicted = energy.predict()
+
+    return labels_predicted != labels_poison
